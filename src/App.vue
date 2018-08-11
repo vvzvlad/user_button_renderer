6e9b94ffb7430a868f0a19d8985906cae5f28e8a
vvzvlad <vvzvlad@gmail.com>
2018-08-11 22:04:40 +0300

    add logic

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
    <v-app>
-      <v-navigation-drawer persistent v-model="drawer" enable-resize-watcher fixed app>
+      <!-- <v-navigation-drawer persistent v-model="drawer" enable-resize-watcher fixed app>
          <v-list>
             <v-list-tile value="true" v-for="(item, i) in items" :key="i">
                <v-list-tile-action>
@@ -11,12 +11,12 @@
                </v-list-tile-content>
             </v-list-tile>
          </v-list>
-      </v-navigation-drawer>
-      <v-toolbar app>
+      </v-navigation-drawer> -->
+      <!-- <v-toolbar app>
          <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
          <v-toolbar-title v-text="title"></v-toolbar-title>
          <v-spacer></v-spacer>
-      </v-toolbar>
+      </v-toolbar> -->
       <v-content>
          <buttons/>
       </v-content>
--- src/components/buttons.vue
+++ src/components/buttons.vue
@@ -2,13 +2,13 @@
    <v-container fluid>
       <v-layout row wrap>
          <v-flex xs12 class="py-2">
-            <v-subheader class="pl-0">Sound</v-subheader>
-            <v-btn-toggle v-model="text">
-               <v-btn flat large block color="primary" value="off">
-                  Off sound
+            <v-subheader class="pl-0">Air conditioner</v-subheader>
+            <v-btn-toggle v-model="conditioner">
+               <v-btn flat large block color="primary" value="off" @click="update('/airref/power/', 'OFF')">
+                  Off conditioner
                </v-btn>
-               <v-btn flat large block color="primary" value="on">
-                  On sound
+               <v-btn flat large block color="primary" value="on" @click="update('/airref/power/', 'ON')">
+                  On conditioner
                </v-btn>
             </v-btn-toggle>
          </v-flex>
@@ -16,8 +16,29 @@
             <v-divider></v-divider>
          </v-flex>
          <v-flex xs12 class="py-2">
-            <v-subheader class="pl-0">Volume</v-subheader>
-            <v-slider v-model="slider"></v-slider>
+            <v-subheader class="pl-0">Light(main room)</v-subheader>
+            <v-btn-toggle v-model="main_light">
+               <v-btn flat large block color="primary" value="off" @click="update('/dimmer/channel/1', '0')">
+                  Off light
+               </v-btn>
+               <v-btn flat large block color="primary" value="on" @click="update('/dimmer/channel/1', '100')">
+                  On light
+               </v-btn>
+            </v-btn-toggle>
+         </v-flex>
+         <v-flex xs12 class="py-2">
+            <v-divider></v-divider>
+         </v-flex>
+         <v-flex xs12 class="py-2">
+            <v-subheader class="pl-0">Light(other rooms)</v-subheader>
+            <v-btn-toggle v-model="main_light">
+               <v-btn flat large block color="primary" value="off" @click="update('/dimmer/channel/2', '0')">
+                  Off light
+               </v-btn>
+               <v-btn flat large block color="primary" value="on" @click="update('/dimmer/channel/2', '100')">
+                  On light
+               </v-btn>
+            </v-btn-toggle>
          </v-flex>
 
       </v-layout>
@@ -25,12 +46,35 @@
 </template>
 
 <script>
+import Vue from "vue";
+import Axios from "axios";
+import VueAxios from "vue-axios";
+Vue.use(VueAxios, Axios);
+
 export default {
   data() {
     return {
       text: "on",
-      slider: 10
+      main_light: "on",
+      conditioner: "on"
     };
+  },
+  methods: {
+    update(topic, value) {
+      Vue.axios
+        .get("http://192.168.88.250:8080/system_bus", {
+          params: {
+            action: "update_value",
+            value: value,
+            topic: topic
+          }
+        })
+        .then(response => {})
+        .catch(error => {
+          console.log(error);
+          this.$refs.snackbar.update("Network error");
+        });
+    }
   }
 };
 </script>
